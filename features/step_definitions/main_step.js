8e0dbc5652637f28e6ef71f5094eba4259d7b2ee
Alexjander Bacalso <janderbacalso@gmail.com>
2016-02-16 23:26:01 +0800

    add question routes
    - add test to check the page title for homepage
    - add test for question page

--- features/step_definitions/main_step.js
+++ features/step_definitions/main_step.js
@@ -7,11 +7,16 @@
  *
  */
 module.exports = function () {
-  this.Given(/^I visit "([^"]*)" page$/, function (text, callback) {
+  this.Given(/^I visit "(\/)" page$/, function (text, callback) {
     this.visit(text, callback);
   });
 
-  this.Then(/^I should see some "([^"]*)"$/, function (text, callback) {
+  this.Then(/^it should have a title "(\/)"$/, function (text, callback) {
+    this.browser.text('title').should.match(new RegExp(text));
+    callback();
+  });
+
+  this.Then(/^should see some "([^"]*)"$/, function (text, callback) {
     this.browser.text('body').should.match(new RegExp(text));
     callback();
   });
